74c061234658797ab6ef4c2946ac4e092f95591d
SepVeneto <sepveneto@qq.com>
2023-02-24 14:00:31 +0800

    ci: sync npmmirror

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,3 +3,9 @@
 pnpm i --frozen-lockfile --registry=https://registry.npmmirror.com
 
 pnpm -r publish --no-git-checks --access public
+
+curl -X PUT https://registry-direct.npmmirror.com/@sepveneto/eslint-config/sync?sync_upstream=true
+curl -X PUT https://registry-direct.npmmirror.com/@sepveneto/eslint-config-basic/sync?sync_upstream=true
+curl -X PUT https://registry-direct.npmmirror.com/@sepveneto/eslint-config-mp/sync?sync_upstream=true
+curl -X PUT https://registry-direct.npmmirror.com/@sepveneto/eslint-config-ts/sync?sync_upstream=true
+curl -X PUT https://registry-direct.npmmirror.com/@sepveneto/eslint-config-vue/sync?sync_upstream=true
